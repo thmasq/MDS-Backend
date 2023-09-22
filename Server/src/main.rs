c4bfacdfd25a56b75df312bad49566dbccf370d4
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-09-22 11:38:18 -0300

    Search func divided in two new funcs

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -31,22 +31,19 @@ struct SearchResults {
     results: Vec<Movie>,
 }
 
-//This is the search function, avaliable at <website_address>/search. It listens for Json requests
-// with a string and returns a response with a JSON file. This function is acyncronous, and the main
-// function will call this function as a factory, the threads are disconnected and non blocking.
-async fn search(query: web::Query<SearchQueryWrapper>, client: web::Data<Client>) -> Result<HttpResponse, Error> {
-    println!("Received search request with query: {:#?}", query);
-
-    if query.q.len() < 3 {
-        // You can adjust the minimum query length
-        return Ok(HttpResponse::Ok().json(SearchResults { results: vec![] }));
-    }
-
-    // Construct the search query using the Meilisearch SDK builder pattern
-    let search_results: meilisearch_sdk::search::SearchResults<Movie> = client
+// This function performs a Meilisearch query based on the provided query string and the Meilisearch
+// client. The function does not perform any query string trimming itself. The query string should
+// be trimmed before calling this function in order to avoid exceeding a certain length.
+// The function returns a Result containing Meilisearch search results or an internal server error
+// if the query fails.
+async fn query_meilisearch(
+    query: &str,
+    client: &Client,
+) -> Result<meilisearch_sdk::search::SearchResults<Movie>, Error> {
+    let search_results = client
         .index("movies")
         .search()
-        .with_query(&query.q)
+        .with_query(query)
         .execute()
         .await
         .map_err(|e| {
@@ -54,31 +51,54 @@ async fn search(query: web::Query<SearchQueryWrapper>, client: web::Data<Client>
             actix_web::error::ErrorInternalServerError("Meilisearch query failed")
         })?;
 
-    println!("Meilisearch search results: {:#?}", search_results);
+    Ok(search_results)
+}
 
+// This function transforms Meilisearch search results into a custom format suitable for the
+// response. It maps Meilisearch hits to a Vec<Movie> and constructs a SearchResults struct for JSON
+// serialization.
+fn transform_results(search_results: meilisearch_sdk::search::SearchResults<Movie>) -> SearchResults {
     let movies: Vec<Movie> = search_results
         .hits
         .iter()
-        .map(|hit| -> Movie {
-            Movie {
-                id: hit.result.id.clone(),
-                title: hit.result.title.clone(),
-                poster: hit.result.poster.clone(),
-                overview: hit.result.overview.clone(),
-                release_date: hit.result.release_date.clone(),
-            }
+        .map(|hit| Movie {
+            id: hit.result.id.clone(),
+            title: hit.result.title.clone(),
+            poster: hit.result.poster.clone(),
+            overview: hit.result.overview.clone(),
+            release_date: hit.result.release_date.clone(),
         })
         .collect();
 
-    println!("Converted movies: {:#?}", movies);
+    SearchResults { results: movies }
+}
 
-    let search_results = SearchResults { results: movies };
+//This is the search function, avaliable at <website_address>/search. It listens for Json requests
+// with a string and returns a response with a JSON file. This function is acyncronous, and the main
+// function will call this function as a factory, the threads are disconnected and non blocking.
+async fn search(query: web::Query<SearchQueryWrapper>, client: web::Data<Client>) -> Result<HttpResponse, Error> {
+    println!("Received search request with query: {:#?}", query);
+
+    // Trim the query to the first 200 characters
+    let trimmed_query = &query.q[..200];
+
+    if trimmed_query.len() < 3 {
+        // You can adjust the minimum query length
+        return Ok(HttpResponse::Ok().json(SearchResults { results: vec![] }));
+    }
+
+    // Query Meilisearch
+    let search_results = query_meilisearch(trimmed_query, &client).await?;
+
+    println!("Meilisearch search results: {:#?}", search_results);
+
+    // Transform results
+    let search_results = transform_results(search_results);
 
     println!("Returning search results as JSON: {:#?}", search_results);
 
     Ok(HttpResponse::Ok().json(search_results))
 }
-
 
 //This function serves the main webpage. Like the search function, each time it is called, the main
 // function will spawn a new disconnected thread.
