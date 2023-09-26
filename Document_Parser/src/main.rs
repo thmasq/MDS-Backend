d4066b8d0a5919d812681b1ec0b4923b31cf3874
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-09-26 13:38:21 -0300

    Adds previous entries to hashset

--- Document_Parser/src/main.rs
+++ Document_Parser/src/main.rs
@@ -24,20 +24,20 @@ struct Data {
 
 
 fn create_folders_if_not_exist() -> Result<(), io::Error> {
-    let folders = ["in", "out", "old"];
+    let folders: [&str; 3] = ["in", "out", "old"];
 
-    for folder in folders.iter() {
+    for folder in &folders {
         let folder_path = Path::new(folder);
         if !folder_path.exists() {
-            println!("The '{}' folder doesn't exist. Create it? (Y/n)", folder);
-            let mut input = String::new();
+            println!("The '{folder}' folder doesn't exist. Create it? (Y/n)");
+            let mut input: String = String::new();
             io::stdin().read_line(&mut input)?;
 
             if input.trim().to_lowercase() == "y" {
                 fs::create_dir(folder_path)?;
-                println!("Created '{}' folder.", folder);
+                println!("Created '{folder}' folder.");
             } else {
-                println!("Folder '{}' does not exist. Exiting.", folder);
+                println!("Folder '{folder}' does not exist. Exiting.");
                 std::process::exit(1);
             }
         }
@@ -51,17 +51,17 @@ fn return_parameters(
     keywords: &[&str],
     existing_titles: &HashSet<String>,
 ) -> Result<(Option<String>, String, Option<i64>), pdf_extract::OutputError> {
-    let formatted_text = format_text(&text);
+    let formatted_text: String = format_text(text);
 
-    println!("Formatted PDF Contents:\n{}", formatted_text);
+    println!("Formatted PDF Contents:\n{formatted_text}");
 
-    let mut found_title = None;
-    let mut found_date = None;
+    let mut found_title: Option<String> = None;
+    let mut found_date: Option<i64> = None;
     for line in formatted_text.lines() {
         if keywords.iter().any(|&keyword| line.contains(keyword)) {
             found_title = Some(line.to_string());
         }
-        if let Some(date) = extract_date(&line) {
+        if let Some(date) = extract_date(line) {
             found_date = Some(date);
         }
         if found_title.is_some() && found_date.is_some() {
@@ -71,7 +71,7 @@ fn return_parameters(
 
     if let Some(ref title) = found_title {
         if existing_titles.contains(title) {
-            println!("Warning: Duplicate entry with title '{}'", title);
+            println!("Warning: Duplicate entry with title '{title}'");
             found_title = None;
             found_date = None;
         }
@@ -81,16 +81,16 @@ fn return_parameters(
 }
 
 fn format_text(input: &str) -> String {
-    let mut formatted_lines = Vec::new();
+    let mut formatted_lines: Vec<String> = Vec::new();
 
     input.lines().for_each(|line| {
         // Remove spaces between characters
-        let re = Regex::new(r"(?<=\S)\s(?=\S)").unwrap();
-        let line_without_spaces = re.replace_all(line, "").to_string();
+        let re: Regex = Regex::new(r"(?<=\S)\s(?=\S)").unwrap();
+        let line_without_spaces: String = re.replace_all(line, "").to_string();
 
         // Replace 2 or more spaces with a single space
-        let re2 = Regex::new(r"\s{2,}").unwrap();
-        let line_with_single_spaces = re2.replace_all(&line_without_spaces, " ").to_string();
+        let re2: Regex = Regex::new(r"\s{2,}").unwrap();
+        let line_with_single_spaces: String = re2.replace_all(&line_without_spaces, " ").to_string();
 
         formatted_lines.push(line_with_single_spaces);
     });
@@ -100,14 +100,15 @@ fn format_text(input: &str) -> String {
 
 fn extract_date(line: &str) -> Option<i64> {
     let re = Regex::new(r"(\d{2})/(\d{2})/(\d{4})").unwrap();
-    if let Ok(Some(captures)) = re.captures(line) {
-        let day = captures.get(1)?.as_str().parse::<u32>().ok()?;
-        let month = captures.get(2)?.as_str().parse::<u32>().ok()?;
-        let year = captures.get(3)?.as_str().parse::<i32>().ok()?;
-        let date = chrono::NaiveDate::from_ymd_opt(year, month, day)?;
-        Some(date.and_hms_opt(0, 0, 0)?.timestamp())
-    } else {
-        None
+    match re.captures(line) {
+        Ok(Some(captures)) => {
+            let day: u32 = captures.get(1)?.as_str().parse::<u32>().ok()?;
+            let month: u32 = captures.get(2)?.as_str().parse::<u32>().ok()?;
+            let year: i32 = captures.get(3)?.as_str().parse::<i32>().ok()?;
+            let date: chrono::NaiveDate = chrono::NaiveDate::from_ymd_opt(year, month, day)?;
+            Some(date.and_hms_opt(0, 0, 0)?.timestamp())
+        }
+        _ => None,
     }
 }
 
@@ -117,9 +118,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let out_folder = Path::new("out");
     let old_folder = Path::new("old");
 
-    let mut existing_titles = HashSet::new();
+    let mut existing_titles: HashSet<String> = HashSet::new();
+    let mut entries: Vec<Entry> = Vec::new();
 
-    let mut entries = Vec::new();
+    // Load existing entries from entries.json if it exists
+    let json_path = out_folder.join("entries.json");
+    if json_path.exists() {
+        let json_str: String = fs::read_to_string(&json_path)?;
+        let existing_entries: Data = serde_json::from_str(&json_str)?;
+        for entry in existing_entries.entries {
+            if let Some(ref title) = entry.title {
+                existing_titles.insert(title.clone());
+            }
+            entries.push(entry);
+        }
+    }
 
     for entry in fs::read_dir(in_folder)? {
         let entry = entry?;
@@ -129,14 +142,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             match return_parameters(&text, &["RESOLUÇÃO", "R E S O L U Ç Ã O", "Header", "Main Title"], &existing_titles) {
                 Ok((title, formatted_text, date)) => {
                     if let Some(ref title_str) = title {
-                        println!("Title found: {}", title_str);
+                        println!("Title found: {title_str}");
                         existing_titles.insert(title_str.clone());
                     } else {
                         println!("No title found");
                     }
 
                     if let Some(date) = date {
-                        println!("Date found: {}", date);
+                        println!("Date found: {date}");
                     } else {
                         println!("No date found");
                     }
@@ -152,8 +165,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                     let entry = Entry {
                         id: title_hash,
-                        title: title,
-                        date: date,
+                        title,
+                        date,
                         content: formatted_text,
                         old_file_name: path.file_name().unwrap().to_string_lossy().into_owned(),
                     };                    
@@ -165,15 +178,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                     fs::rename(&path, old_path)?;
                 }
                 Err(err) => {
-                    eprintln!("Error: {:?}", err);
+                    eprintln!("Error: {err:?}");
                 }
             }
         }
     }
 
-    let json_str = serde_json::to_string_pretty(&entries)?;
+    let data: Data = Data { entries };
 
-    let json_path = out_folder.join("entries.json");
+    let json_str: String = serde_json::to_string_pretty(&data)?;
+
     fs::write(&json_path, json_str)?;
 
     Ok(())
