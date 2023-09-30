fd477227e66c6cf5aff106ecac5b0b3c43dccb8f
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-09-30 17:16:57 -0300

    Reads Brazilian date format

--- Document_Parser/src/main.rs
+++ Document_Parser/src/main.rs
@@ -58,11 +58,16 @@ fn return_title(formatted_text: &str, keywords: &[&str]) -> Option<String> {
 }
 
 fn return_date(formatted_text: &str) -> Option<i64> {
+    if let Some(date) = extract_portuguese_date(formatted_text) {
+        return Some(date);
+    }
+
     for line in formatted_text.lines() {
         if let Some(date) = extract_date(line) {
             return Some(date);
         }
     }
+
     None
 }
 
@@ -91,6 +96,61 @@ fn return_parameters(
     Ok((result_title, text.to_string(), result_date, is_duplicate))
 }
 
+fn extract_portuguese_date(line: &str) -> Option<i64> {
+    // Define month names in Portuguese (case-insensitive)
+    let month_names = ["janeiro",
+        "fevereiro",
+        "março",
+        "abril",
+        "maio",
+        "junho",
+        "julho",
+        "agosto",
+        "setembro",
+        "outubro",
+        "novembro",
+        "dezembro"];
+
+    // Regular expression to match the Portuguese date format
+    let re = Regex::new(r"(\d{1,2})\s*de\s*([^\d\s]+)\s*de\s*(\d{2,4})")
+        .expect("Invalid Regular Expression for Portuguese Date.");
+
+    if let Ok(Some(captures)) = re.captures(line) {
+        let day: u32 = captures.get(1)?.as_str().parse::<u32>().ok()?;
+        let month_str = captures.get(2)?.as_str().to_lowercase();
+        let year_str = captures.get(3)?.as_str();
+
+        // Convert the Portuguese month name to a numeric month
+        let month: Option<u32> = month_names
+            .iter()
+            .position(|&m| m.to_lowercase() == month_str)
+            .and_then(|idx| idx.try_into().ok());
+
+        if let Some(month) = month {
+            // Determine the year format (2 or 4 digits)
+            let year: i32 = if year_str.len() == 2 {
+                let current_year = chrono::Local::now().year() % 100; // Get the current two-digit year
+                let year: i32 = year_str.parse::<i32>().ok()?;
+                if year <= current_year {
+                    // If the year is less than or equal to the current two-digit year, assume it's in the current
+                    // century
+                    2000 + year
+                } else {
+                    // Otherwise, assume it's in the previous century
+                    1900 + year
+                }
+            } else {
+                year_str.parse::<i32>().ok()?
+            };
+
+            let date: chrono::NaiveDate = chrono::NaiveDate::from_ymd_opt(year, month, day)?;
+            return Some(date.and_hms_opt(0, 0, 0)?.timestamp());
+        }
+    }
+
+    None
+}
+
 fn extract_date(line: &str) -> Option<i64> {
     let re = Regex::new(r"(\d{1,2})/(\d{1,2})/(\d{2,4})").expect("Invalid Regular Expression for Date.");
     let captures = re.captures(line);
@@ -117,10 +177,10 @@ fn extract_date(line: &str) -> Option<i64> {
         };
 
         let date: chrono::NaiveDate = chrono::NaiveDate::from_ymd_opt(year, month, day)?;
-        Some(date.and_hms_opt(0, 0, 0)?.timestamp())
-    } else {
-        None
+        return Some(date.and_hms_opt(0, 0, 0)?.timestamp());
     }
+
+    None
 }
 
 fn get_link(path: &Path) -> Result<String, Box<dyn Error>> {
