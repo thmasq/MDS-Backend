9a9a29e222a44096167cffba79701683c0542920
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2023-11-24 12:57:22 -0300

    Initial implementation of PopulateDB program

---
+++ PopulateDB/src/main.rs
@@ -0,0 +1,378 @@
+#![allow(non_snake_case)]
+
+use chrono::{DateTime, NaiveDate, Utc};
+use rand::Rng;
+use sqlx::mysql::MySqlConnectOptions;
+use sqlx::{query, MySql, Pool};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
+use crossterm::{event::{self, Event, KeyCode}, execute, terminal::{EnterAlternateScreen, LeaveAlternateScreen}, };
+use std::io::stdout;  
+
+#[derive(Debug)]
+struct User {
+    email: Option<String>,
+    userName: Option<String>,
+    token: Option<String>,
+}
+
+#[derive(Debug)]
+struct Document {
+    docName: Option<String>,
+    link: Option<String>,
+    content: Option<String>,
+    docKey: Option<String>,
+    creationDate: Option<i64>,
+}
+
+#[derive(Debug)]
+struct Favorite {
+    favoriteId: i64,
+    userToken: String,
+    documentId: String,
+}
+
+#[derive(Debug)]
+enum MyError {
+   SqlxError(sqlx::Error),
+   IoError(std::io::Error),
+   InvalidChoice(String),
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::SqlxError(e) => write!(f, "Database error: {e}"),
+            Self::InvalidChoice(s) => write!(f, "Invalid choice: {s}"),
+            Self::IoError(a) => write!(f, "IO error: {a}"),
+        }
+    }
+}
+
+impl Error for MyError {}
+
+impl From<sqlx::Error> for MyError {
+    fn from(err: sqlx::Error) -> Self {
+        Self::SqlxError(err)
+    }
+}
+
+impl From<std::io::Error> for MyError {
+    fn from(err: std::io::Error) -> Self {
+        Self::IoError(err)
+    }
+ }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pool = Pool::connect_with(
+        MySqlConnectOptions::new()
+            .host("localhost")
+            .port(3306)
+            .username("aaaa")
+            .password("aaaaaaaaa")
+            .database("aaaaaa"),
+    )
+    .await?;
+
+    loop {
+        print!("\n");
+        println!("1. Create a new document");
+        println!("2. Create a new user");
+        println!("3. Favorite an item as a user");
+        println!("4. Exit");
+
+        let choice: i32 = input("Enter your choice: ");
+
+        match choice {
+            1 => create_new_document(&pool).await?,
+            2 => create_new_user(&pool).await?,
+            3 => favorite_item_as_user(&pool).await?,
+            4 => break,
+            _ => println!("Invalid choice. Please enter a valid option."),
+        }
+    }
+
+    Ok(())
+}
+
+async fn create_new_document(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
+    let docName: String = input("Enter document name: ");
+    let link: String = input("Enter document link: ");
+    let content: String = input("Enter document content: ");
+    let docKey: String = input("Enter document key: ");
+
+    // Prompt the user for the creation date
+    let creationDate: NaiveDate = input("Enter creation date (YYYY-MM-DD): ");
+    let creationDate =
+        DateTime::<Utc>::from_naive_utc_and_offset(creationDate.and_hms_opt(0, 0, 0).expect("Impossible date"), Utc);
+    let unix_epoch_time = creationDate.timestamp();
+
+    let document = Document {
+        docName: Some(docName.clone()),
+        link: Some(link.clone()),
+        content: Some(content.clone()),
+        docKey: Some(docKey.clone()),
+        creationDate: Some(unix_epoch_time),
+    };
+
+    // Insert the new document into the DOCUMENT table
+    let result = sqlx::query!(
+        "INSERT INTO DOCUMENT (docName, link, creationDate, content, docKey) VALUES (?, ?, ?, ?, ?)",
+        document.docName,
+        document.link,
+        document.creationDate,
+        document.content,
+        document.docKey
+    )
+    .execute(pool)
+    .await?;
+
+    if result.rows_affected() > 0 {
+        println!("Document created successfully!");
+    } else {
+        println!("Failed to create document.");
+    }
+
+    Ok(())
+}
+
+async fn create_new_user(pool: &Pool<MySql>) -> Result<(), sqlx::Error> {
+    let email: String = input("Enter user email: ");
+    let userName: String = input("Enter user name: ");
+    let token: String = input("Enter user token: ");
+
+    let user = User {
+        email: Some(email.clone()),
+        userName: Some(userName.clone()),
+        token: Some(token.clone()),
+    };
+
+    // Insert the new user into the USER table
+    let result = query!(
+        "INSERT INTO USER (email, userName, token) VALUES (?, ?, ?)",
+        user.email,
+        user.userName,
+        user.token
+    )
+    .execute(pool)
+    .await?;
+
+    if result.rows_affected() > 0 {
+        println!("User created successfully!");
+    } else {
+        println!("Failed to create user.");
+    }
+
+    Ok(())
+}
+
+async fn favorite_item_as_user(pool: &Pool<MySql>) -> Result<(), MyError> {
+    let users = list_users(pool).await?;
+    let documents = list_documents(pool).await?;
+
+    loop {
+        print!("\n");
+        println!("1. Create favorite");
+        println!("2. List users");
+        println!("3. List documents");
+        println!("4. Exit");
+
+        let choice: i32 = input("Enter your choice: ");
+        println!();
+
+        let result = match choice {
+            1 => create_favorite(pool, &users, &documents).await,
+            2 => print_users(&users),
+            3 => print_documents(&documents),
+            4 => break,
+            _ => {
+                println!("\nInvalid choice. Please enter a valid option.");
+                Ok(())
+            },
+        };
+
+        // Propagate any errors
+        result?;
+    }
+
+    Ok(())
+}
+
+async fn create_favorite(
+    pool: &Pool<MySql>,
+    users: &HashMap<Option<String>, Option<String>>,
+    documents: &HashMap<Option<String>, Option<String>>,
+) -> Result<(), MyError> {
+    let (_, userToken) = select_from_map::<_, _, fn(&Option<String>) -> &String>("Select a user:", users)?;
+    let (_, documentId) = select_from_map::<_, _, fn(&Option<String>) -> &String>("Select a document:", documents)?;
+    print!("\n");
+
+    // Generate a unique favoriteId
+    let mut rng = rand::rngs::OsRng;
+let mut favoriteId: i64;
+
+// Loop until a unique favoriteId is generated
+loop {
+   favoriteId = rng.gen();
+
+   // Check if the generated favoriteId already exists in the database
+   let count: i64 = sqlx::query_scalar!("SELECT COUNT(*) FROM favorites WHERE favoriteId = ?", favoriteId)
+       .fetch_one(pool)
+       .await?;
+
+        if count == 0 {
+            break; // Exit the loop if the favoriteId is unique
+        }
+    }
+
+    let favorite = Favorite {
+        favoriteId,
+        userToken: userToken.clone().expect("No user token Found."),
+        documentId: documentId.clone().expect("No document ID found."),
+    };
+
+    // Insert the new favorite into the FAVORITES table
+    let result = sqlx::query!(
+        "INSERT INTO favorites (favoriteId, userToken, documentId) VALUES (?, ?, ?)",
+        favorite.favoriteId,
+        favorite.userToken,
+        favorite.documentId
+    )
+    .execute(pool)
+    .await?;
+
+    if result.rows_affected() > 0 {
+        println!("Favorite added successfully!");
+    } else {
+        println!("\nFailed to add favorite.");
+    }
+
+    Ok(())
+}
+
+async fn list_users(pool: &Pool<MySql>) -> Result<HashMap<Option<String>, Option<String>>, MyError> {
+    let users = sqlx::query_as!(User, "SELECT userName, email, token FROM USER")
+        .fetch_all(pool)
+        .await?;
+ 
+    let mut user_map = HashMap::new();
+ 
+    for user in users {
+        user_map.insert(user.email.clone(), user.token.clone());
+    }
+ 
+    Ok(user_map)
+ }
+ 
+ async fn list_documents(pool: &Pool<MySql>) -> Result<HashMap<Option<String>, Option<String>>, MyError> {
+    let documents = sqlx::query_as!(
+        Document,
+        "SELECT docName, docKey, content, creationDate, link FROM DOCUMENT"
+    )
+    .fetch_all(pool)
+    .await?;
+ 
+    let mut document_map = HashMap::new();
+ 
+    for document in documents {
+        document_map.insert(document.docName.clone(), document.docKey.clone());
+    }
+ 
+    Ok(document_map)
+ }
+ 
+
+fn select_from_map<T, U, V>(prompt: &str, choices: &HashMap<T, U>) -> Result<(T, U), MyError>
+where
+    T: std::cmp::Eq + std::hash::Hash + std::clone::Clone + std::fmt::Debug,
+    U: std::clone::Clone + std::fmt::Debug,
+    V: Fn(&U) -> &String,
+{
+    println!("{prompt}");
+
+    for (i, choice) in choices.iter().enumerate() {
+        println!("{}. {:?}", i + 1, choice.0);
+    }
+
+    let index: usize = input("Enter your choice (number): ");
+
+    if index > 0 && index <= choices.len() {
+        let selected_key = choices.keys().nth(index - 1).expect("No key");
+        let selected_value = choices.get(selected_key).expect("No value");
+        Ok((selected_key.clone(), selected_value.clone()))
+    } else {
+        Err(MyError::InvalidChoice(
+            "Invalid choice. Please enter a valid option.".to_string(),
+        ))
+    }
+}
+
+fn print_users(users: &HashMap<Option<String>, Option<String>>) -> Result<(), MyError> {
+    execute!(stdout(), EnterAlternateScreen)?;
+ 
+    println!("\nUsers:");
+    for (i, user) in users.iter().enumerate() {
+        println!("{}. {:?}", i + 1, user.0);
+    }
+    println!();
+ 
+    loop {
+        if event::poll(std::time::Duration::from_millis(100))? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                   KeyCode::Char('q') => break,
+                   _ => {}
+                }
+            }
+        }
+    }
+ 
+    execute!(stdout(), LeaveAlternateScreen)?;
+ 
+    Ok(())
+ }
+ 
+ fn print_documents(documents: &HashMap<Option<String>, Option<String>>) -> Result<(), MyError> {
+    execute!(stdout(), EnterAlternateScreen)?;
+ 
+    println!("\nDocuments:");
+    for (i, document) in documents.iter().enumerate() {
+        println!("{}. {:?}", i + 1, document.0);
+    }
+    println!();
+ 
+    loop {
+        if event::poll(std::time::Duration::from_millis(100))? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                   KeyCode::Char('q') => break,
+                   _ => {}
+                }
+            }
+        }
+    }
+ 
+    execute!(stdout(), LeaveAlternateScreen)?;
+ 
+    Ok(())
+ } 
+
+fn input<T>(prompt: &str) -> T
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    loop {
+        println!("{prompt}");
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim().parse() {
+            Ok(value) => return value,
+            Err(err) => println!("Error: {err:?}"),
+        }
+    }
+}
